b5956513450ae775cb1fc0eb01d90511c9f7b523
Mohamed Hedi Sekma <sekma.mohamed.hedi@gmail.com>
2023-10-02 22:57:05 +0100

    style main

---
+++ js/animation.js
@@ -0,0 +1,7 @@
+
+document.getElementById("servicesTitle").classList.add("animation0");
+function animation(){
+    document.getElementById("servicesTitle").classList.remove("animation0");
+    document.getElementById("servicesTitle").classList.add("animation");
+
+}
