51d1f5b7d99619dc84ef7b039c1a937db5df29b5
Mohamed Hedi Sekma <sekma.mohamed.hedi@gmail.com>
2023-10-06 21:31:04 +0100

    style main

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,16 @@
+window.onscroll=function(){animation()};
 
-document.getElementById("servicesTitle").classList.add("animation0");
 function animation(){
-    document.getElementById("servicesTitle").classList.remove("animation0");
-    document.getElementById("servicesTitle").classList.add("animation");
-
+        if(document.documentElement.scrollTop>150){
+        document.getElementById("servicesTitle").classList.add("animation");
+        setTimeout(function(){
+                document.querySelector(".servicesTitle-container").style.opacity="10";
+        },1000)
+}   
+        if(document.documentElement.scrollTop>900){
+                document.getElementById("staffTitle").classList.add("animation"); 
+                setTimeout(function(){
+                        document.querySelector(".staffTitle-container").style.opacity="10";
+                },1000)
+        }  
 }
