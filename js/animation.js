724ae79e8a88ff7bc814747a944438d13d5b2c22
Mohamed Hedi Sekma <sekma.mohamed.hedi@gmail.com>
2023-10-13 23:44:49 +0100

    finished tablet version

--- js/animation.js
+++ js/animation.js
@@ -1,16 +1,24 @@
 window.onscroll=function(){animation()};
 
 function animation(){
-        if(document.documentElement.scrollTop>100){
-        document.getElementById("servicesTitle").classList.add("slideUp");
-        setTimeout(function(){
-                document.querySelector(".servicesTitle-container").style.opacity="10";
-        },1000)
-}   
-        if(document.documentElement.scrollTop>700){
-                document.getElementById("staffTitle").classList.add("slideUp"); 
+        if(document.documentElement.clientWidth>991){
+                if(document.documentElement.scrollTop>200){
+                document.getElementById("servicesTitle").classList.add("slideUp");
                 setTimeout(function(){
-                        document.querySelector(".staffTitle-container").style.opacity="10";
+                        document.querySelector(".servicesTitle-container").style.opacity="10";
                 },1000)
-        }  
+                }   
+                if(document.documentElement.scrollTop>800){
+                        document.getElementById("staffTitle").classList.add("slideUp"); 
+                        setTimeout(function(){
+                                document.querySelector(".staffTitle-container").style.opacity="10";
+                        },1000)
+                }  
+        }else{
+                document.getElementById("servicesTitle").classList.add("visible"); 
+                document.getElementById("staffTitle").classList.add("visible");
+                
+                document.querySelector(".servicesTitle-container").style.opacity="10";
+                document.querySelector(".staffTitle-container").style.opacity="10";
+        }
 }
